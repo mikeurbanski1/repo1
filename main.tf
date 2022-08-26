7974eb5c7f1005b059074c182fa16442cd57532d
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-08-26 10:09:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-resource "aws_s3_bucket" "repo_1_bucket" {
-  bucket = "abc"
-}
-
-module "local_module" {
-  source = "./module"
-}
-
-module "local_module_2" {
-  source = "./module"
-}
-
-module "public_reg_module" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
-  name     = "my-table"
-  hash_key = "id"
-
-  attributes = [
-    {
-      name = "id"
-      type = "N"
-    }
-  ]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "staging"
-  }
-
-  server_side_encryption_enabled = false
+resource "aws_ebs_volume" "v" {
+  
 }
