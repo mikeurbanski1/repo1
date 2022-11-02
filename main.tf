9bb322c20ee289bd3266c6a652a55484817ec733
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-11-02 13:03:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,10 @@ resource "alicloud_vpc" "vpc" {
 resource "alicloud_network_acl" "acl" {
   
 }
+
+resource "aws_s3_bucket" "traced" {
+  bucket = "bc-sa-drift-1"
+  tags = {
+    yor_trace = "6264ed3d-03a6-4665-9400-17e842bd8bf8"
+  }
+}
